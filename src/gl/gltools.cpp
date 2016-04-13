fdfddc7db72942399421fcf188913cbb65a7ba1c
trizer <mrtrizer@gmail.com>
2016-04-13 23:45:37 +0300

    Optimized checkOpenGLerror function.

--- src/gl/gltools.cpp
+++ src/gl/gltools.cpp
@@ -9,15 +9,15 @@ namespace GLTools {
 //TODO: How to get an error description without GLUT?
 void checkOpenGLerror(const char * file, const char * func, int line) {
     GLenum errCode = glGetError();
-    std::stringstream ss;
-    ss << "[ERROR] " << file << ' ' << line << ' ' << func << std::endl;
-    if(errCode != GL_NO_ERROR)
+    if(errCode != GL_NO_ERROR) {
+        std::stringstream ss;
+        ss << "[ERROR] " << file << ' ' << line << ' ' << func << std::endl;
         LOGE("%s",ss.str().data());
 #ifdef QT_DEBUG
-    ss << "Text: " << reinterpret_cast<const char *>(gluErrorString(errCode)) << std::endl;
-    if(errCode != GL_NO_ERROR)
+        ss << "Text: " << reinterpret_cast<const char *>(gluErrorString(errCode)) << std::endl;
         throw std::runtime_error(ss.str());
 #endif
+    }
 }
 
 }
