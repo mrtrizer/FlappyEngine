df822c41643148e72e19196993b43761e14bed13
Denis Zdorovtsov <mrtrizer@gmail.com>
2016-06-30 15:11:18 +0300

    Fixed GLViewShape.

--- src/gl/glviewshape.cpp
+++ src/gl/glviewshape.cpp
@@ -61,11 +61,11 @@ GViewRect::GViewRect(float width, float height):
     rect(GL_TRIANGLE_STRIP){
 
     std::vector<GLTools::Vertex> vertexList({
-                                                {0,0},
-                                                {0,height},
-                                                {width,0},
-                                                {width,height}
-                                            });
+                  {-width / 2,-height / 2},
+                  {-width / 2,height / 2},
+                  {width / 2,-height / 2},
+                  {width / 2,height / 2}
+              });
     rect.addVBO<GLTools::Vertex>(vertexList.data(),
                                  static_cast<int>(vertexList.size()) * sizeof(GLTools::Vertex),
                                  GL_FLOAT,
