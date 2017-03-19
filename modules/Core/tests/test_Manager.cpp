3ae686353d07ead47b7a9744e2e9721480a01c0e
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-03-19 14:37:21 +0300

    Tested manager add/remove events.

--- modules/Core/tests/test_Manager.cpp
+++ modules/Core/tests/test_Manager.cpp
@@ -4,10 +4,65 @@
 #include <memory>
 
 #include <Manager.h>
+#include <Entity.h>
 
 #include "TestManager.h"
 
 using namespace flappy;
 using namespace std;
 
-// There are nothing to test. Tested in previous tests.
+TEST_CASE("Add/Remove manager events") {
+    auto entityRoot = std::make_shared<Entity>();
+    auto entityMiddle = entityRoot->createEntity();
+    auto componentMiddle = entityMiddle->createComponent<Component>();
+
+    bool onAddedCalled = false;
+
+    componentMiddle->events()->subscribeIn([&onAddedCalled](AManager::OnManagerAdded e) {
+        if (auto manager = e.castTo<TestManager>())
+            onAddedCalled = true;
+    });
+
+    bool onRemovedCalled = false;
+
+    componentMiddle->events()->subscribeIn([&onRemovedCalled](AManager::OnManagerRemoved e) {
+        if (auto manager = e.castTo<TestManager>())
+            onRemovedCalled = true;
+    });
+
+    auto testManager = entityRoot->createManager<TestManager>();
+    REQUIRE(onAddedCalled == true);
+    REQUIRE(onRemovedCalled == false);
+
+    entityRoot->removeManager(testManager);
+    REQUIRE(onRemovedCalled == true);
+}
+
+TEST_CASE("Add/Remove component events") {
+    auto entityRoot = std::make_shared<Entity>();
+    auto entityMiddle = entityRoot->createEntity();
+    auto testManager = entityRoot->createManager<TestManager>();
+
+    auto componentMiddle = std::make_shared<Component>();
+
+    bool onAddedCalled = false;
+
+    componentMiddle->events()->subscribeIn([&onAddedCalled](AManager::OnManagerAdded e) {
+        if (auto manager = e.castTo<TestManager>())
+            onAddedCalled = true;
+    });
+
+    bool onRemovedCalled = false;
+
+    componentMiddle->events()->subscribeIn([&onRemovedCalled](AManager::OnManagerRemoved e) {
+        if (auto manager = e.castTo<TestManager>())
+            onRemovedCalled = true;
+    });
+
+    entityMiddle->addComponent(componentMiddle);
+    REQUIRE(onAddedCalled == true);
+    REQUIRE(onRemovedCalled == false);
+
+    entityMiddle->removeComponent(componentMiddle);
+    REQUIRE(onRemovedCalled == true);
+}
