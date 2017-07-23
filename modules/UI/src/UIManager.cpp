9da1501688f029534f7d54adc64cd345a8a90a5d
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-07-23 12:20:45 +0300

    TouchComponent processing.

--- modules/UI/src/UIManager.cpp
+++ modules/UI/src/UIManager.cpp
@@ -1,29 +1,47 @@
 #include "UIManager.h"
 
 #include <TouchInputManager.h>
+#include <TransformComponent.h>
+
+#include "TouchComponent.h"
 
 namespace flappy {
 
-UIManager::UIManager():Manager({TouchInputManager::id()}) {
-    events()->subscribeIn([](TouchInputManager::TouchDownEvent e) {
-
+UIManager::UIManager():Manager({TouchInputManager::id()})
+{
+    events()->subscribeIn([this](TouchInputManager::TouchDownEvent e) {
+        auto touchComponent = findTouchComponentInPoint(e.pos);
+        if (touchComponent != nullptr)
+            touchComponent->touchDown(e.pos, e.index);
     });
-    events()->subscribeIn([](TouchInputManager::TouchUpEvent e) {
-
+    events()->subscribeIn([this](TouchInputManager::TouchUpEvent e) {
+        auto touchComponent = findTouchComponentInPoint(e.pos);
+        if (touchComponent != nullptr)
+            touchComponent->touchUp(e.pos, e.index);
     });
-    events()->subscribeIn([](TouchInputManager::TouchMoveEvent e) {
-
+    events()->subscribeIn([this](TouchInputManager::TouchMoveEvent e) {
+        auto touchComponent = findTouchComponentInPoint(e.pos);
+        if (touchComponent != nullptr)
+            touchComponent->touchMove(e.pos, e.index);
     });
+}
+
+SafePtr<TouchComponent> UIManager::findTouchComponentInPoint(glm::vec2 point)
+{
+    for (auto touchComponent : m_touchComponentList) {
+        if (touchComponent->testPoint(point))
+            return touchComponent;
+    }
 }
 
 void UIManager::registerTouchComponent(SafePtr<TouchComponent> touchComponent)
 {
-
+    m_touchComponentList.push_back(touchComponent);
 }
 
 void UIManager::unregisterTouchComponent(SafePtr<TouchComponent> touchComponent)
 {
-
+    m_touchComponentList.remove(touchComponent);
 }
 
 } // flappy
--- modules/UI/src/UIManager.h
+++ modules/UI/src/UIManager.h
@@ -12,6 +12,11 @@ public:
     UIManager();
     void registerTouchComponent(SafePtr<TouchComponent> touchComponent);
     void unregisterTouchComponent(SafePtr<TouchComponent> touchComponent);
+
+private:
+    SafePtr<TouchComponent> findTouchComponentInPoint(glm::vec2 point);
+
+    std::list<SafePtr<TouchComponent>> m_touchComponentList;
 };
 
 } // flappy
