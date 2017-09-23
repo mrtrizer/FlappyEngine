83d8c4a807194571eaa8f29c3f8bda3820c71316
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-09-23 19:42:03 +0300

    Forbidded adding of multiple components to one entity.

--- modules/Core/src/Entity.h
+++ modules/Core/src/Entity.h
@@ -168,6 +168,9 @@ void Entity::addComponent(std::shared_ptr<ComponentT> component)
 {
     static_assert(isBaseOf<ComponentBase, ComponentT>(), "Type must be a descendant of Component");
 
+    if (findComponent<ComponentT>() != nullptr)
+        throw std::runtime_error("Multiple components of the same type can't be attached to an entity.");
+
     if (component->entity() != nullptr)
         throw std::runtime_error("Can't add a component to several entities.");
     component->setParentEntity(shared_from_this());
