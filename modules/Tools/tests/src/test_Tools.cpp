ac449c268d59492c46641d608b63efacc3f0b541
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-10-10 08:17:07 +0300

    Added test for format function

--- modules/Tools/tests/src/test_Tools.cpp
+++ modules/Tools/tests/src/test_Tools.cpp
@@ -12,3 +12,8 @@ TEST_CASE( "Tools::isPowOfTwo()") {
     REQUIRE(Tools::isPowOfTwo(8) == true);
     REQUIRE(Tools::isPowOfTwo(13) == false);
 }
+
+TEST_CASE( "Tools::format()") {
+    auto str = Tools::format("Test %d test %s", 5, "123");
+    REQUIRE(str == "Test 5 test 123");
+}
