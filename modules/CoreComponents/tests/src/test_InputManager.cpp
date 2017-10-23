01e977c51715518055b00374d1adf3d7e91813cf
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-10-23 09:05:50 +0300

    Fixed CoreComponents input manager test. Fixed Ogg res factory.

--- modules/CoreComponents/tests/src/test_InputManager.cpp
+++ modules/CoreComponents/tests/src/test_InputManager.cpp
@@ -3,6 +3,7 @@
 
 #include <memory>
 
+#include <Entity.h>
 #include <MouseInputManager.h>
 
 using namespace flappy;
@@ -10,37 +11,41 @@ using namespace std;
 using namespace glm;
 
 TEST_CASE( "MouseInputManager::isMouseDown()") {
-    MouseInputManager inputManager;
-    inputManager.setMouseDown(MouseInputManager::MouseButton::LEFT, {0.0f, 0.0f});
-    REQUIRE(inputManager.isMouseDown() == false);
-    inputManager.events()->post(ComponentBase::UpdateEvent(1.0f));
-    REQUIRE(inputManager.isMouseDown() == true);
-    inputManager.events()->post(ComponentBase::UpdateEvent(1.0f));
-    REQUIRE(inputManager.isMouseDown() == false);
+    auto entity = std::make_shared<Entity>();
+    auto inputManager = entity->createComponent<MouseInputManager>();
+    inputManager->setMouseDown(MouseInputManager::MouseButton::LEFT, {0.0f, 0.0f});
+    REQUIRE(inputManager->isMouseDown() == false);
+    inputManager->events()->post(ComponentBase::UpdateEvent(1.0f));
+    REQUIRE(inputManager->isMouseDown() == true);
+    inputManager->events()->post(ComponentBase::UpdateEvent(1.0f));
+    REQUIRE(inputManager->isMouseDown() == false);
 }
 
 TEST_CASE( "MouseInputManager::isMouseUp()") {
-    MouseInputManager inputManager;
-    inputManager.setMouseUp(MouseInputManager::MouseButton::LEFT, {0.0f, 0.0f});
-    REQUIRE(inputManager.isMouseUp() == false);
-    inputManager.events()->post(ComponentBase::UpdateEvent(1.0f));
-    REQUIRE(inputManager.isMouseUp() == true);
-    inputManager.events()->post(ComponentBase::UpdateEvent(1.0f));
-    REQUIRE(inputManager.isMouseUp() == false);
+    auto entity = std::make_shared<Entity>();
+    auto inputManager = entity->createComponent<MouseInputManager>();
+    inputManager->setMouseUp(MouseInputManager::MouseButton::LEFT, {0.0f, 0.0f});
+    REQUIRE(inputManager->isMouseUp() == false);
+    inputManager->events()->post(ComponentBase::UpdateEvent(1.0f));
+    REQUIRE(inputManager->isMouseUp() == true);
+    inputManager->events()->post(ComponentBase::UpdateEvent(1.0f));
+    REQUIRE(inputManager->isMouseUp() == false);
 }
 
 TEST_CASE( "MouseInputManager::isMousePressed()") {
-    MouseInputManager inputManager;
-    REQUIRE(inputManager.isMousePressed() == false);
-    inputManager.setMouseDown(MouseInputManager::MouseButton::LEFT, {0.0f, 0.0f});
-    REQUIRE(inputManager.isMousePressed() == true);
-    inputManager.setMouseUp(MouseInputManager::MouseButton::LEFT, {0.0f, 0.0f});
-    REQUIRE(inputManager.isMousePressed() == false);
+    auto entity = std::make_shared<Entity>();
+    auto inputManager = entity->createComponent<MouseInputManager>();
+    REQUIRE(inputManager->isMousePressed() == false);
+    inputManager->setMouseDown(MouseInputManager::MouseButton::LEFT, {0.0f, 0.0f});
+    REQUIRE(inputManager->isMousePressed() == true);
+    inputManager->setMouseUp(MouseInputManager::MouseButton::LEFT, {0.0f, 0.0f});
+    REQUIRE(inputManager->isMousePressed() == false);
 }
 
 TEST_CASE( "MouseInputManager::getMousePos()") {
-    MouseInputManager inputManager;
-    REQUIRE(inputManager.mousePos() == vec2(0,0));
-    inputManager.setMousePos(MouseInputManager::MouseButton::LEFT, {10, 10});
-    REQUIRE(inputManager.mousePos() == vec2(10,10));
+    auto entity = std::make_shared<Entity>();
+    auto inputManager = entity->createComponent<MouseInputManager>();
+    REQUIRE(inputManager->mousePos() == vec2(0,0));
+    inputManager->setMousePos(MouseInputManager::MouseButton::LEFT, {10, 10});
+    REQUIRE(inputManager->mousePos() == vec2(10,10));
 }
