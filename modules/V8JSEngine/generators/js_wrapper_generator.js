697c7d58bc6bef654f977ecfd19d26afe02adfb4
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-11-19 00:35:01 +0300

    Support of timestamp caching to not generate wrappers for cpp classes every time.

--- modules/V8JSEngine/generators/js_wrapper_generator.js
+++ modules/V8JSEngine/generators/js_wrapper_generator.js
@@ -8,10 +8,18 @@ function isComponentCpp(fileName) {
             || (fileName.indexOf("Manager") != -1));
 }
 
-function getSourceList(context) {
+function getSourceList(context, cacheSubDir) {
     const utils = context.require("./utils");
     const modules = context.require("./modules");
     const path = require('path');
+    const fs = require('fs');
+    const fse = context.require("fs-extra");
+
+    const timestampCachePath = path.join(cacheSubDir, "timestampCache.json");
+    let timestamps = {};
+    try {
+        timestamps = fse.readJsonSync(timestampCachePath);
+    } catch (e) {}
 
     let sourceList = [];
 
@@ -23,49 +31,61 @@ function getSourceList(context) {
         const files = utils.readDirs(srcDir);
         for (const i in files) {
             const filePath = files[i];
-            const fileName = path.parse(filePath).base;
-            if (isComponentCpp(fileName)) {
-                console.log(filePath);
-                sourceList.push(filePath);
+            const lastModifTime = Math.floor(fs.statSync(filePath).mtime);
+            if (!(filePath in timestamps) || timestamps[filePath] != lastModifTime) {
+                console.log(filePath + " - Changed : " + lastModifTime);
+                const fileName = path.parse(filePath).base;
+                if (isComponentCpp(fileName)) {
+                    console.log(filePath);
+                    sourceList.push(filePath);
+                }
+                timestamps[filePath] = lastModifTime;
             }
         }
     }
+
+    fse.outputJson(timestampCachePath, timestamps, {spaces: 4});
+
     return sourceList.join(" ");
+}
+
+function call(command, cwd) {
+    const childProcess = require("child_process");
+    childProcess.execSync(command, {"cwd": cwd, stdio: "inherit"});
 }
 
 module.exports.generate = function (context, resConfig, resSrcDir, cacheSubDir) {
     const fs = require('fs');
     const path = require('path');
     const fse = context.require("fs-extra");
-
-    const sourceList = getSourceList(context);
-
-    function call(command, cwd) {
-        const childProcess = require("child_process");
-        childProcess.execSync(command, {"cwd": cwd, stdio: "inherit"});
-    }
+    // Build
     const buildDir = path.join(resSrcDir, "../generators/v8WrapperGenerator/src/build");
     console.log("Build dir: " + buildDir);
-    // Make automatic searching of DCMAKE_PREFIX_PATH
-    // Search -extra-arg automatically too
-    // Run flappy gen cmake and CMake before wrapper generation
+    // TODO: Make automatic searching of DCMAKE_PREFIX_PATH
+    // TODO: Search -extra-arg automatically too
+    // TODO: Run flappy gen cmake and CMake before wrapper generation
     const llvmDir = "/usr/local/Cellar/llvm/5.0.0/"
     const cmakePath = path.join(llvmDir, "lib/cmake/llvm");
-    const clangIncludes1 = path.join(llvmDir, "include/c++/v1");
-    const clangIncludes2 = path.join(llvmDir, "lib/clang/5.0.0/include");
     fse.mkdirsSync(buildDir);
-    const outputDir = path.join(context.projectRoot, "flappy_cache/V8JSEngine");
-    fse.mkdirsSync(path.join(outputDir, "wrappers"));
     call(`cmake -G \"Ninja\" -DCMAKE_PREFIX_PATH=\"${cmakePath}\" ..`, buildDir);
     call(`ninja`, buildDir);
-    const generateCommand = `./js_wrapper_generator `
-                            + ` -extra-arg \"-I${clangIncludes1}\"`
-                            + ` -extra-arg \"-I${clangIncludes2}\"`
-                            + ` -p \"${context.targetOutDir}\"`
-                            + ` --output \"${outputDir}\"`
-                            + ' ' + sourceList;
-    console.log("Generation command: ", generateCommand);
-    call(generateCommand, buildDir);
+    // Generate
+    const sourceList = getSourceList(context, cacheSubDir);
+    if (sourceList.length > 0) {
+        const outputDir = path.join(context.projectRoot, "flappy_cache/V8JSEngine");
+        fse.mkdirsSync(path.join(outputDir, "wrappers"));
+        const clangIncludes1 = path.join(llvmDir, "include/c++/v1");
+        const clangIncludes2 = path.join(llvmDir, "lib/clang/5.0.0/include");
+
+        const generateCommand = `./js_wrapper_generator `
+                                + ` -extra-arg \"-I${clangIncludes1}\"`
+                                + ` -extra-arg \"-I${clangIncludes2}\"`
+                                + ` -p \"${context.targetOutDir}\"`
+                                + ` --output \"${outputDir}\"`
+                                + ' ' + sourceList;
+        console.log("Generation command: ", generateCommand);
+        call(generateCommand, buildDir);
+    }
 
     return [];
 };
