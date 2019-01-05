eeb09f712e929697a8057304583ff81a41cf141a
Denis Zdorovtsov <mrtrizer@gmail.com>
2019-01-05 19:46:14 +0300

    Removed old modules. Some configuration update.

--- modules/V8JSEngine/tests/res_src/JsAccessComponent.js
+++
@@ -1,7 +0,0 @@
-class JsAccessComponent extends Component {
-
-    callCalcSum(val1, val2) {
-        return this.entity().jsComponent('TestComponent1').calcSum(val1, val2);
-    }
-
-}
--- modules/V8JSEngine/tests/res_src/TestComponent1.js
+++
@@ -1,7 +0,0 @@
-class TestComponent1 extends Component {
-
-    calcSum(val1, val2) {
-        return val1 + val2;
-    }
-
-}
